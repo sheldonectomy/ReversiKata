b78460f7c5e4b4ef1089480c89881930d8fb98e3
Alan Campbell <alan.vine@outlook.com>
2019-04-25 08:54:18 +0100

    Checkpoint

---
+++ ReversiKata01/ReversiKata01/Board.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversiKata01
+{
+
+    public class SquareContent
+    {
+        public SquareContent(int x, int y, string content = ".")
+        {
+            X = x;
+            Y = y;
+            Content = content;
+        }
+        public int X { get; }
+        public int Y { get; }
+        public string Content { get; set; }
+    }
+
+    public class Board
+    {
+        List<SquareContent> _squareContents = new List<SquareContent>();
+        string _nextToMove;
+
+        public Board()
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    _squareContents.Add(new SquareContent(x, y, "."));
+                }
+            }
+            SetSquareContent(3, 3, "B");
+            SetSquareContent(4, 3, "W");
+            SetSquareContent(3, 4, "W");
+            SetSquareContent(4, 4, "B");
+            _nextToMove = "B";
+        }
+
+        public string GetSquareContent(int x, int y)
+        {
+            return _squareContents.First(a => a.X == x && a.Y == y).Content;
+        }
+
+        public void SetSquareContent(int x, int y, string content)
+        {
+            _squareContents.First(a => a.X == x && a.Y == y).Content = content;
+        }
+
+        public string GraphicalText
+        {
+            get
+            {
+                var output = new StringBuilder();
+                for (int y = 0; y < 8; y++)
+                {
+                    for (int x = 0; x < 8; x++)
+                    {
+                        output.Append(GetSquareContent(x, y));
+                    }
+                    output.Append(
+                        y == 7 ? string.Empty : Environment.NewLine);
+                }
+                return output.ToString();
+            }
+        }
+
+        public string NextToMove
+        {
+            get => _nextToMove;
+            set => _nextToMove = value;
+        }
+
+        public List<SquareContent> CandidateSquares
+        {
+            get
+            {
+                var candidates = new List<SquareContent>();
+
+                return candidates;
+            }
+        }
+
+        private bool isCandidateSquare(int x, int y)
+        {
+            bool result = false;
+            if(GetSquareContent(x, y) == ".")
+            {
+                for(int xTransform = -1; xTransform <= 1; xTransform++)
+                {
+                    for(int yTransform = -1; yTransform <= 1; yTransform++)
+                    {
+                        
+                        if(
+                            (xTransform == 0 && yTransform ==0)
+                            || xTransform + x < 0 || xTransform + x > 7
+                            || y + yTransform < 0 || y + yTransform > 7
+                            )
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ ReversiKata01/ReversiKata01/ReversiTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversiKata01
+{
+    [TestFixture]
+    public class ReversiTests
+    {
+
+        Board _board;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _board = new Board();
+        }
+
+        [Test]
+        public void NewBoardHasCorrectSetup()
+        {
+            var output = new StringBuilder();
+            for(int y = 0; y < 8; y++)
+            {
+               for(int x = 0; x < 8; x++)
+                {
+                    output.Append(_board.GetSquareContent(x, y));
+                }
+                output.Append(
+                    y == 7 ? string.Empty : Environment.NewLine);
+            }
+            Assert.AreEqual(exampleBoardAtStart(), output.ToString());
+        }
+
+        [Test]
+        public void CanDisplayGraphicalTextString()
+        {
+            Assert.AreEqual(
+                exampleBoardAtStart(),
+                _board.GraphicalText);
+        }
+
+        [Test]
+        public void BlackIsFirstToMove()
+        {
+            Assert.AreEqual("B", _board.NextToMove);
+        }
+
+        [Test]
+        public void CanFindCandidateSquares()
+        {
+            var candidates = new List<SquareContent>
+            {
+                new SquareContent(4, 2, "C"),
+                new SquareContent(5, 2, "C"),
+                new SquareContent(5, 3, "C"),
+                new SquareContent(2, 4, "C"),
+                new SquareContent(2, 5, "C"),
+                new SquareContent(3, 5, "C")
+            };
+            Assert.AreEqual(candidates, _board.CandidateSquares);
+        }
+
+        private string exampleBoardAtStart()
+        {
+            return
+@"........
+........
+........
+...BW...
+...WB...
+........
+........
+........";
+        }
+    }
+}
